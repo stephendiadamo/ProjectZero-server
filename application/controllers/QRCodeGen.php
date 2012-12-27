1b79c7341ac39cb7eaf22aa1d96faec90523b02e
stephendiadamo <stephen.diadamo@gmail.com>
2012-12-26 23:00:00 -0500

    SD - Added some prescription functionality - Using an 21x21 grid of binary to store QR code... Need to find a better way to do this

--- application/controllers/QRCodeGen.php
+++ application/controllers/QRCodeGen.php
@@ -3,8 +3,14 @@ include "application/libraries/phpqrcode/qrlib.php";
 
 class QRCodeGen extends CI_Controller {
 	public function generate(){		
-		QRcode::png($_GET["code"]);
+		QRcode::png($_GET["code"], false, 'L', 4, 2);
 	}
+	
+	public function rawGenerate($code){		
+		$temp = QRcode::text($code, false, 'L', 4, 2);
+		return json_encode($temp);
+	}
+	
 	
 	// DATABASE STUFF: 	
 	// ADD NEW USER
@@ -13,6 +19,23 @@ class QRCodeGen extends CI_Controller {
 		$this->load->model("users");		
 		$this->users->addNewUser($_GET["user"] , $_GET["account_type_id"]);
 	}	
+	
 	// GET DRUGS BY USER_ID
+	public function getDrugsByUser(){	
+		$this->load->model("users");
+		$results = $this->users->getDrugs($_GET["user_id"]);		
+		echo json_encode($results->result_array());
+	}
+	
+	public function addNewPrescription(){
+	
+		$user_id = $_GET["user_id"];
+		$doctor_id = $_GET["doctor_id"];
+		$drug = $_GET["drug"];
+		$qrcode = $this->rawGenerate($doctor_id . ";" . $user_id . ";" . $drug);
+		
+		$this->load->model("users");
+		$this->users->addNewDrug($user_id, $doctor_id, $drug, $qrcode);	
+	}	 
 }
 ?>
--- application/models/users.php
+++ application/models/users.php
@@ -16,9 +16,22 @@ class Users extends CI_Model {
 	}
 	
 	// TODO: WRITE THIS
-	function getDrugs($user_id){
+	function getDrugs($user_id){	
+		$query_string = "SELECT * 
+						 FROM prescriptions
+						 WHERE user_id = " . $user_id;
+		return $this->db->query($query_string);		
 	}
-
+	
+	function addNewDrug($user_id, $doctor_id, $drug, $qrcode){
+		$data = array(
+			'drug_name'=>$drug,
+			'user_id'=>$user_id,
+			'doctor_id'=>$doctor_id,
+			'qrcode'=>$qrcode
+		);	
+		return $this->db->insert('prescriptions', $data);
+	}
 }
 
 ?>
