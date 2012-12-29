daba93fcfc6c531ad4165fcff15410b0d119c2b1
stephendiadamo <stephen.diadamo@gmail.com>
2012-12-29 18:14:48 -0500

    SD - Added more functions - Added a bit of error handling

--- application/controllers/QRCodeGen.php
+++ application/controllers/QRCodeGen.php
@@ -2,6 +2,7 @@
 include "application/libraries/phpqrcode/qrlib.php";
 
 class QRCodeGen extends CI_Controller {
+
 	public function generate(){		
 		QRcode::png($_GET["code"], false, 'L', 4, 2);
 	}
@@ -10,32 +11,61 @@ class QRCodeGen extends CI_Controller {
 		$temp = QRcode::text($code, false, 'L', 4, 2);
 		return json_encode($temp);
 	}
-	
-	
-	// DATABASE STUFF: 	
-	// ADD NEW USER
-	
-	public function addUser(){		
-		$this->load->model("users");		
-		$this->users->addNewUser($_GET["user"] , $_GET["account_type_id"]);
-	}	
-	
-	// GET DRUGS BY USER_ID
-	public function getDrugsByUser(){	
+		
+	// DATABASE STUFF: 		
+	public function fetchUsers(){
 		$this->load->model("users");
-		$results = $this->users->getDrugs($_GET["user_id"]);		
-		echo json_encode($results->result_array());
+		$data = $this->users->fetchUsers();
+		if ($data->result_array() != null){
+			echo json_encode($data->result_array());
+		}
 	}
 	
-	public function addNewPrescription(){
-	
-		$user_id = $_GET["user_id"];
-		$doctor_id = $_GET["doctor_id"];
-		$drug = $_GET["drug"];
-		$qrcode = $this->rawGenerate($doctor_id . ";" . $user_id . ";" . $drug);
-		
+	// Add a new user	
+	public function addUser(){		
 		$this->load->model("users");
-		$this->users->addNewDrug($user_id, $doctor_id, $drug, $qrcode);	
-	}	 
+		if (isset($_GET["user"]) && isset($_GET["account_type_id"])){
+			$this->users->addNewUser($_GET["user"], $_GET["account_type_id"]);
+		}
+	}
+	
+	// Get info by user_id
+	public function getUser(){	
+		$this->load->model("users");
+		if (isset($_GET["user_id"])){
+			$results = $this->users->getPrescData($_GET["user_id"]);
+			if ($results->result_array() != null){		
+				echo json_encode($results->result_array());
+			}
+		}
+	}
+	
+	public function retrieveQRCode(){
+		$this->load->model("users");
+		if (isset($_GET["user_id"]) && isset($_GET["drug"])){
+			$results = $this->users->getQRCode($_GET["user_id"], $_GET["drug"]);
+			if ($results->result_array() != null){		
+				echo json_encode($results->result_array());
+			}
+		}
+	}
+	
+	public function addPresc(){
+		if (isset($_GET["user_id"]) && isset($_GET["doctor_id"]) && isset($_GET["drug"])){
+			$user_id = $_GET["user_id"];
+			$doctor_id = $_GET["doctor_id"];
+			$drug = $_GET["drug"];
+			$qrcode = $this->rawGenerate($doctor_id . ";" . $user_id . ";" . $drug);
+			$this->load->model("users");
+			$this->users->addNewDrug($user_id, $doctor_id, $drug, $qrcode);	
+		}
+	}
+	
+	public function removeUser(){
+		if (isset($_GET["user_id"])){
+			$this->load->model("users");
+			$this->users->removeUser($_GET["user_id"]);
+		}
+	}	
 }
 ?>
--- application/models/users.php
+++ application/models/users.php
@@ -7,6 +7,10 @@ class Users extends CI_Model {
 		$this->load->database();
 	}
 	
+	function fetchUsers(){
+		return $this->db->get("users");	
+	}
+	
 	function addNewUser($name, $account_type_id){
 		$data = array(
 			'name'=>$name,
@@ -15,12 +19,20 @@ class Users extends CI_Model {
 		return $this->db->insert('users', $data);  
 	}
 	
-	// TODO: WRITE THIS
-	function getDrugs($user_id){	
+	function getPrescData($user_id){	
 		$query_string = "SELECT * 
 						 FROM prescriptions
 						 WHERE user_id = " . $user_id;
 		return $this->db->query($query_string);		
+	}
+	
+	function getQRCode($user_id, $drug){
+		$query_string = "SELECT qrcode
+				         FROM prescriptions
+				         WHERE user_id = " . $user_id .
+				         " AND drug_name = '" . $drug . "'";
+		return $this->db->query($query_string);  
+	
 	}
 	
 	function addNewDrug($user_id, $doctor_id, $drug, $qrcode){
@@ -32,6 +44,10 @@ class Users extends CI_Model {
 		);	
 		return $this->db->insert('prescriptions', $data);
 	}
+	
+	function removeUser($user_id){
+		$this->db->delete('users', array('id' => $user_id));
+	}	
 }
 
 ?>
