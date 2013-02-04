ed5736ef861b695eca5a2beb56f55d5dcf458766
stephendiadamo <stephen.diadamo@gmail.com>
2013-02-03 22:19:10 -0500

    SD Reg page updates - Modified date selector - Modified OHIP num generate

--- application/views/newuser.php
+++ application/views/newuser.php
@@ -6,12 +6,21 @@
 		$this->load->helper('url');
 		$this->load->helper('form');
 	?>
+	
 	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.js"></script>
 	<script>
+
 	var jq;	
 	// Something is conflicting with jquery 
 	jq=jQuery.noConflict();		
 	// Jquery loadup
+	
+	jq(document).ready(function() {
+		var rand_ohip = ("" + Math.random()).substring(2,6);
+		document.getElementById("ohip_num").value = rand_ohip;
+	});
+
+  	
 	jq (function() {
 		jq('#reg').submit(function() {
 			var formdata = jq('#reg').serialize();
@@ -20,16 +29,21 @@
 				url: sub_url,
 				success: function(data){
 					if (data != "FAIL"){
-						window.open("http://www.pollev.com");	
+						var ohip = jq("#ohip_num").val();
+						var password = jq("#pass").val();
+						alert("Success! Login Info: \nOHIP: " + ohip + "\nPassword: " + password );
+						jq('.field').val('');
+						var rand_ohip = ("" + Math.random()).substring(2,6);
+						document.getElementById("ohip_num").value = rand_ohip;	
 					} else {
 						alert("User already exists in the database.");
+						jq('.field').val('');
 					}
 				},
 				error: function(xhr, ajaxOptions, thrownError){	
 					alert("Error: " + xhr.responseText);
 				}
-			});
-			jq('.field').val('');		
+			});			
 			return false;
 		});
 	});
@@ -38,25 +52,27 @@
 	<style type="text/css">
 	
 		body{
-			padding: 100px;
-			width: 1000px;
-			margin: 0 auto;	
+			padding: 75px;
+			width: 50%;
+			margin: 0 auto;
 		}
 		#content{
-			width: 70%;
-			margin: 0 auto;	
+			width: 100%;
+			margin: 0 auto;
 		}
 		form{
-			width: 50%;
-			margin: 0 auto;	
+			width: 100%;
+			margin: 0 auto;
 		}
 		
 		h1{
-			text-align: center;
+			text-align: center;					
+			width: 100%;	
 		}
-		
+				
 		input, select{
 			padding: 5px;
+			margin: 5px;
 			width: 150px;
 		}
 		#sub{
@@ -70,11 +86,14 @@
 </head>
 
 <body>
+	
 	<div id="content">
-	<h1>Register User</h1>
-
+	<table>
+	<tr><h1>Register User</h1></tr>
+	<tr>	
 	<form id="reg">
 		<table>
+		
 		<tr>
 			<td>First name: </td> 
 			<td><input class="field" required type="text" name="first_name"/></td>
@@ -92,14 +111,18 @@
   				<option value="3">Pharmacist</option>
 			</select> </td>
 		</tr>
-		<tr><td> Password: </td><td><input class="field" required type="password" name="password"/></td></tr>
+		<tr><td> Password: </td><td><input class="field" id="pass" required type="password" name="password"/></td></tr>
 		<tr><td> Repeat password: </td><td><input class="field" required type="password" name="rep_password"/></td></tr>
-		<tr><td>OHIP ID: </td><td><input required class="field" type="text" name="ohip"/></td></tr>
-		<tr><td>Birthday: </td><td><input required class="field" type="text" name="birthday"/></td></tr>
-		<tr><td><input id="sub" type="submit" value="Register User"/></td></tr>
+		<tr><td>OHIP ID: </td><td><input required id="ohip_num" class="field" type="text" name="ohip"/></td></tr>
+		<tr><td>Birthday: </td><td><input required id="datepicker" class="field" type="date" name="birthday"/></td></tr>
+		<tr><td>
+			<input id="sub" type="submit" value="Register User"/>
+		</td></tr>
 		</table>	
 		<input type="hidden" name="description" value="">
 	</form>
+	</tr>
+	</table>
 	</div>
 </body>
 
